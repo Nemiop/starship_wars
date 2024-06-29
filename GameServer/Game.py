67b7e4953e6bda48be41d368b7feba8adede0687
Nemiop <timchenkoruslan97@gmail.com>
2024-06-29 16:55:07 +0200

    Update move abstractions

--- GameServer/Game.py
+++ GameServer/Game.py
@@ -1,27 +1,42 @@
 from abc import abstractmethod
 from dataclasses import dataclass
+from typing import NewType, List
 
 import numpy as np
 
 
-@dataclass
-class Position:
-    x: int
-    y: int
+class Coordinates:
+    coordinates: List[int]
+
+    @property
+    def x(self) -> int:
+        return self.coordinates[0]
+
+    @x.setter
+    def x(self, new_x: int):
+        self.coordinates[0] = new_x
+
+    @property
+    def y(self) -> int:
+        return self.coordinates[1]
+
+    @y.setter
+    def y(self, new_y: int):
+        self.coordinates[1] = new_y
 
 
 @dataclass
-class Direction:
-    angle: float
+class Position(Coordinates):
+    pass
 
 
-class Velocity:
-    speed_x: int
-    speed_y: int
+@dataclass
+class Velocity(Coordinates):
+    pass
 
 
-class Rotation:
-    angular_speed: float
+Direction = NewType("Direction", float)
+Rotation = NewType("Rotation", float)
 
 
 class GameObject:
@@ -41,8 +56,8 @@ class SpaceShip(GameObject):
         self.rotation: Rotation = rotation
 
     def move(self):
-        x_new = self.position.x + self.velocity.speed_x
-        y_new = self.position.y + self.velocity.speed_y
+        x_new = self.position.x + self.velocity.x
+        y_new = self.position.y + self.velocity.y
         self.position = Position(x_new, y_new)
 
         self.direction = self.direction.angle + self.rotation.angular_speed
